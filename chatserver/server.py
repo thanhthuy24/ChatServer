e1b71a2adc2acd08a8b117d3ee248f76718e7c29
master-piece <hothanhthuy11.2003@gmail.com>
2023-11-26 12:02:29 +0700

    add server

--- chatserver/server.py
+++ chatserver/server.py
@@ -1,39 +1,105 @@
+# import socket
+#
+# #192.168.2.7
+# HOST = "127.0.0.1"
+# SERVER_PORT = 51263
+# FORMAT = "utf8"
+#
+# def recvList(socket):
+#     list = []
+#     item = conn.recv(1024).decode(FORMAT)
+#     while (item != "end"):
+#
+#         list.append(item)
+#         #response
+#         conn.sendall(item.encode(FORMAT))
+#         item = conn.recv(1024).decode(FORMAT)
+#
+#     return list
+#
+# #socket.AF_INET -- dùng để thiết lập ipv4
+# #socket.SOCK_STREAM -- dùng để thiết lập TCP
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# #bind.. -- lắng nghe địa chỉ address và port
+# s.bind((HOST, SERVER_PORT))
+# s.listen()
+#
+# print("SERVER SIDE")
+# print("server: ", HOST, SERVER_PORT)
+# print("Waiting for client")
+#
+# try:
+#     conn, addr = s.accept()
+#
+#     print("client address: ", addr)
+#     print("conn: ", conn.getsockname())
+#
+#     msg = None
+#     while(msg != "x"):
+#         msg = conn.recv(1024).decode(FORMAT)
+#         print("client ", addr, "says ", msg)
+#         msg = input("Server response: ")
+#         conn.sendall(msg.encode(FORMAT))
+#
+#         if (msg == "list"):
+#             conn.sendall(msg.encode(FORMAT))
+#             list = recvList(conn)
+#
+#             print("received: ")
+#             print(list)
+#
+# except:
+#     print("Error")
+#
+# print("End")
+# s.close()
+#
+#
 import socket
+import threading
 
-#192.168.2.7
+import rsa
+
 HOST = "127.0.0.1"
-SERVER_PORT = 51263
-FORMAT = "utf8"
+PORT = 9999
+SIZE = 1024
 
-#socket.AF_INET -- dùng để thiết lập ipv4
-#socket.SOCK_STREAM -- dùng để thiết lập TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-#bind.. -- lắng nghe địa chỉ address và port
-s.bind((HOST, SERVER_PORT))
-s.listen()
-
-print("SERVER SIDE")
-print("server: ", HOST, SERVER_PORT)
-print("Waiting for client")
-
-try:
-    conn, addr = s.accept()
-
-    print("client address: ", addr)
-    print("conn: ", conn.getsockname())
-
-    msg = None
-    while(msg != "x"):
-        msg = conn.recv(1024).decode(FORMAT)
-        print("client ", addr, "says ", msg)
-        msg = input("Server response: ")
-        conn.sendall(msg.encode(FORMAT))
-
-except:
-    print("Error")
-
-print("End")
-s.close()
+public_key, private_key = rsa.newkeys(SIZE)
+public_partner = None
 
 
+
+choice = input("Do you want to host (1) or to connect (2): ")
+
+if choice == "1":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+
+    client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(SIZE))
+
+elif choice == "2":
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(SIZE))
+    client.send(public_key.save_pkcs1("PEM"))
+
+else:
+    exit()
+
+
+def sending_messages(c):
+    while True:
+        message = input()
+        c.send(rsa.encrypt(message.encode(), public_partner))
+        print("You: " + message)
+
+def receiving_messages(c):
+    while True:
+        print("Partner: " + rsa.decrypt(c.recv(SIZE), private_key).decode())
+
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
---
+++ chatserver/socket/client.py
@@ -0,0 +1,46 @@
+import socket
+
+HOST = '127.0.0.1'
+SERVER_PORT = 51263
+FORMAT = 'utf8'
+
+def sendList(client, list):
+
+    for item in list:
+        client.sendall(item.encode(FORMAT))
+        #waiting response
+        client.recv(1024)
+
+    msg = "end"
+    client.sendall(msg.encode(FORMAT))
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print("CLIENT SIDE")
+
+try:
+    client.connect((HOST, SERVER_PORT))
+    print("client address: ", client.getsockname())
+
+    list = ["duchieuvn", "15", "nam"]
+
+    msg = None
+    while(msg != "x"):
+        msg = input("Talk: ")
+        # gửi lệnh đi
+        client.sendall(msg.encode(FORMAT))
+        #msg = client.recv(1024).decode(FORMAT)
+        #print("Server response: ", msg)
+
+        if (msg == "list"):
+            # wait response
+            client.recv(1024)
+            sendList(client, list)
+
+
+except:
+    print("Error")
+
+print("End")
+client.close()
